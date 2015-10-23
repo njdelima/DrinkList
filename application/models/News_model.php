ef4930d7faf3c2d8a03c7237aec2fa07c51ba54b
Neeraj Joshua DeLima <neeraj.delima@gmail.com>
2015-10-23 16:19:43 -0400

    Adding news articles

--- application/models/News_model.php
+++ application/models/News_model.php
@@ -13,5 +13,17 @@
 			$query = $this->db->get_where('news', array('slug' => $slug));
 			return $query->row_array();
 		}
+
+		public function set_news() {
+			$slug = url_title($this->input->post('title'), 'dash', TRUE);
+
+			$data = array(
+				'title' => $this->input->post('title'),
+				'slug' => $slug,
+				'text' => $this->input->post('text')
+			);
+
+			return $this->db->insert('news', $data);
+		}
 	}
 ?>
